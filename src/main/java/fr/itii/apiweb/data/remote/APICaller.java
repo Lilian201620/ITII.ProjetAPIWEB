8547f9649cb200fdd3fe4df6b0ee8fb9175e445d
nicolas <nicolas.sen092@gmail.com>
2026-01-26 11:50:37 +0100

    ApiCaller : Methode getCommunesByName(string) créée

--- src/main/java/fr/itii/apiweb/data/remote/APICaller.java
+++ src/main/java/fr/itii/apiweb/data/remote/APICaller.java
@@ -1,15 +1,40 @@
 package fr.itii.apiweb.data.remote;
 
-public class APICaller {
-    private final String apiUrl;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 
-    public APICaller(String apiUrl) {
-        this.apiUrl = apiUrl;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class APICaller {
+    private final String API_URL = "https://geo.api.gouv/communes";
+    public APICaller() {
     }
 
-    public String request() {
-        return null; // Pour l'instant
+    public JsonNode getCommunesByName(String commune){
+        ObjectMapper objectMapper = new ObjectMapper();
+        HttpClient client = HttpClient.newHttpClient();
+        String encodedCommune = URLEncoder.encode(commune, StandardCharsets.UTF_8);
+        String url = API_URL
+                + "?nom=" + encodedCommune
+                + "&fields=nom,code,codeDepartement,codeRegion,codesPostaux,population"
+                + "&limit=10";
 
-        //
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .GET()
+                    .build();
+            HttpResponse<String> response;
+
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return objectMapper.readTree(response.body());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
