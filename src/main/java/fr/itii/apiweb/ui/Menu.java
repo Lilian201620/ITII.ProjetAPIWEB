fa1c24a98b6726889c7560004a66b7403bd9b7e1
enzod <enzodubois2064@gmail.com>
2026-01-27 16:05:24 +0100

    menu v1

--- src/main/java/fr/itii/apiweb/ui/Menu.java
+++ src/main/java/fr/itii/apiweb/ui/Menu.java
@@ -18,77 +18,32 @@ public class Menu {
     Scanner sc = new Scanner(System.in);
 
     public String showMenu() {
-        System.out.println(BOLD + "1. Rechercher API" + RESET);
+        System.out.println("\n" + BOLD + "1. Rechercher API" + RESET);
         System.out.println(BOLD + "2. Rechercher dans la DataBase" + RESET);
         System.out.println(BOLD + "3. Supprimer la database" + RESET);
         System.out.println(BOLD + "4. Quitter" + RESET);
-        System.out.print(prompt());
+        System.out.print(">");
 
         return sc.nextLine().trim();
     }
 
     public String showMenuAPI() {
-        System.out.println(BOLD + "1. Elargir la recherche" + RESET);   //Plus de ligne
+        System.out.println("\n" + BOLD + "1. Elargir la recherche" + RESET);   //Plus de ligne
         System.out.println(BOLD + "2. Sauvegarder" + RESET);            //Save dans DB
         System.out.println(BOLD + "3. Nouvelle recherche" + RESET);     //Nouvelle API
         System.out.println(BOLD + "4. Retour" + RESET);                 //Retour au menu principal
-        System.out.print(prompt());
+        System.out.print(">");
 
         return sc.nextLine().trim();
     }
 
     public String showMenuDB() {
-        System.out.println(BOLD + "1. Sauvegarder" + RESET);
+        System.out.println("\n" + BOLD + "1. Sauvegarder" + RESET);
         System.out.println(BOLD + "2. Nouvelle recherche" + RESET);
         System.out.println(BOLD + "3. Retour" + RESET);
-        System.out.print(prompt());
+        System.out.print(">");
 
         return sc.nextLine().trim();
     }
 
-    public void switchPrincipal() {
-        switch (showMenu()) {
-            case "1" -> //searchApi();
-                        //show(List, nombre);
-                        //switchAPI
-            case "2" -> //searchDatabase();
-                        //show(List, nombre);
-                        //switchDB
-            case "3" -> //deleteDatabase();
-            case "4" -> {
-                info("Fin.");
-                System.exit(0);
-            }
-            default -> switchPrincipal();
-        }
-    }
-
-    public void switchAPI() {
-        switch (showMenuAPI()) {
-            case "1" -> {
-                //searchAPI() -> showAPI()
-
-            }
-            case "2" -> //saveLastResults();
-            case "3" -> {
-                return;
-            }
-            case "4" -> System.exit(0);
-            default -> //
-        }
-    }
-
-    public switchDB() {
-        switch (showMenuDB()) {
-            case "1" -> {
-                return;
-            }
-            case "2" ->
-            case "3" -> System.exit(0);
-            default -> //
-        }
-    }
-
-
-}
 }
