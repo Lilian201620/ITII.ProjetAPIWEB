9a2e0e7073fca0a92ae77afd6a3de85950c5612e
3xt4z_ <74177752+DarksLive@users.noreply.github.com>
2026-01-28 09:06:03 +0100

    option3_avecEtablissements (#2)
    * feat: Modif tests
    
    * etablissement comme commune
    
    * feat: ajout models DB
    
    * feat: ajout models DB
    
    * EtablissementApiCaller :
    Appels byNomCommune, byCodeCommune, byCodeDepartement
    Tests EtablissementApiCaller
    
    * +1 Test EtablissementApiCaller
    
    * etablissement comme commune
    
    * etablissement comme commune
    
    * etablissement comme commune
    
    * +1 Test EtablissementApiCaller
    
    * feat: ajout models DB
    
    ---------
    
    Co-authored-by: Lilian <lilian150505@gmail.com>
    Co-authored-by: nicolas <nicolas.sen092@gmail.com>

---
+++ src/main/java/fr/itii/apiweb/domain/models/dbModels/CommunesCol.java
@@ -0,0 +1,11 @@
+package fr.itii.apiweb.domain.models.dbModels;
+
+public enum CommunesCol extends Col {
+    id,
+    nom,
+    codecommune,
+    codedepartement,
+    codepostal,
+    coderegion,
+    population
+}
