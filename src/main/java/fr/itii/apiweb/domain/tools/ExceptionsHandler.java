bf113f7e9d644e70f572fce1b0e31c8a9fd98019
Nathan - kgtg7 <killergtg7@gmail.com>
2026-01-28 14:44:50 +0100

    feat: modif exceptionHandler

--- src/main/java/fr/itii/apiweb/domain/tools/ExceptionsHandler.java
+++ src/main/java/fr/itii/apiweb/domain/tools/ExceptionsHandler.java
@@ -7,16 +7,20 @@ import java.io.InterruptedIOException;
 import java.sql.SQLException;
 
 public class ExceptionsHandler {
+    private static final String RED = "\u001B[31m";
+    private static final String RESET = "\u001B[0m";
+    private static final String BOLD = "\u001B[1m";
 
     public static void handleException(Exception exception){
-        if (exception instanceof IOException) {
-            System.out.println("Un problème est survenu lors de la requête. Verifiez votre connexion et réessayez");
-        }else if (exception instanceof InterruptedException){
-            System.out.println("Un problème est survenu lors de la requête. Verifiez votre connexion et réessayez");
-        } else if (exception instanceof SQLException){
-            System.out.println("Un problème est survenu avec la base de donnée, veuillez réessayer");
-        }else {
-            System.out.println("Un problème inconnu est survenu, veuillez réessayer");
+        switch (exception) {
+            case IOException ioException ->
+                    System.out.println(RED + BOLD + "Un problème est survenu lors de la requête à l'API Web. Vérifiez votre connexion et réessayez" + RESET);
+            case InterruptedException interruptedException ->
+                    System.out.println(RED + BOLD + "Un problème est survenu lors de la requête à l'API Web. Vérifiez votre connexion et réessayez" + RESET);
+            case SQLException _ ->
+                    System.out.println(RED + BOLD + "Un problème est survenu avec la base de données, vérifiez le serveur SQL et réessayez" + RESET);
+            case null, default ->
+                    System.out.println(RED + BOLD + "Un problème inconnu est survenu, veuillez réessayer" + RESET);
         }
     }
 
