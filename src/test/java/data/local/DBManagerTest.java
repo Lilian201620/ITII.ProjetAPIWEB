3bcbdb4060f415cd9282733711328617e2b10adc
Nathan - kgtg7 <killergtg7@gmail.com>
2026-01-28 12:08:14 +0100

    feat: remplacement DBManager par NewDBManager

--- src/test/java/data/local/DBManagerTest.java
+++ src/test/java/data/local/DBManagerTest.java
@@ -1,11 +1,5 @@
 package data.local;
-import fr.itii.apiweb.domain.models.Commune;
-import org.junit.jupiter.api.Test;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 public class DBManagerTest {
 
