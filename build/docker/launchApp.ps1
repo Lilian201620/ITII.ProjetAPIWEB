72451b4ef2bafc7e30dcbb68dc02ea1d42207461
Nathan - kgtg7 <killergtg7@gmail.com>
2026-01-30 13:45:01 +0100

    fix: modif script ps1

--- build/docker/launchApp.ps1
+++ build/docker/launchApp.ps1
@@ -1,12 +1,9 @@
 $serviceName = "app"
-$containerName = "apiweb_tool"
-$imageName = "myapp:1.0"
 $dockerComposeFile = "docker-compose_postgresql.yml"
 
 try {
-    docker build -t $imageName .
     docker-compose.exe -f $dockerComposeFile up -d
-    docker-compose.exe -f $dockerComposeFile attach app
+    docker-compose.exe -f $dockerComposeFile attach $serviceName
 } catch {
     Write-Error "Docker n'est pas installé."
 }
