b79ab1b5c04d00161de70ae6c283f09d5d65c5d1
Nathan - kgtg7 <killergtg7@gmail.com>
2026-01-30 14:39:37 +0100

    fix: modif adresse DB

--- build/docker/launchApp.ps1
+++ build/docker/launchApp.ps1
@@ -1,7 +1,9 @@
 $serviceName = "app"
+$containerName = "lilian2016/apiwebtool:latest"
 $dockerComposeFile = "docker-compose_postgresql.yml"
 
 try {
+    docker.exe pull $containerName
     docker-compose.exe -f $dockerComposeFile up -d
     docker-compose.exe -f $dockerComposeFile attach $serviceName
 } catch {
